946eaba3ea21bf77f4dd953bb70f0d60cad0c7e5
Julienr85 <julienr85@gmail.com>
2016-11-08 17:29:55 +0100

    Save

---
+++ save.js
@@ -0,0 +1,8 @@
+function savePicture()
+		{
+    	canvas_sauvegarde = canvas.toDataURL(); // Récupération du canvas
+    	canvas_sauvegarde = canvas_sauvegarde.replace("image/png", "image/octet-stream");
+    	document.location.href = canvas_sauvegarde;
+    	window.location.href = canvas_sauvegarde;
+		}
+     
